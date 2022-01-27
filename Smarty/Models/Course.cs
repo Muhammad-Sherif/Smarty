a364865a208b596230498bdac1bec6058482ee09
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-01-27 16:59:31 +0200

    Add intial models

---
+++ Smarty/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smarty.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public string Description{ get; set; }
+        public DayOfWeek Day { get; set; }
+        public TimeSpan Time { get; set; }
+        public Guid RegisterCode{ get; set; }
+        public Guid AccessCode{ get; set; }
+
+    }
+}
