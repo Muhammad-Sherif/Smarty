b1e7f96e9cd517819f2b9f818dbd04292b1f200f
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-01-28 16:48:50 +0200

    Add ef configurations

---
+++ Smarty/Data/SmartyDBContext/SmartyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Smarty.Data.Models;
+
+namespace Smarty.Data.SmartyDBContext
+{
+    public class SmartyDbContext : DbContext
+    {
+        public SmartyDbContext(DbContextOptions<SmartyDbContext> contextOptions) : base(contextOptions)
+        {
+
+        }
+        
+    }
+}
--- Smarty/Program.cs
+++ Smarty/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Smarty.Data.SmartyDBContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<SmartyDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Default")));
+
 
 var app = builder.Build();
 
