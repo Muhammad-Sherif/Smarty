dbdbec900a3161f200fb607b6d59b4ffd1195447
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-01-31 13:34:56 +0200

    Add automapper

--- Smarty/Program.cs
+++ Smarty/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<SmartyDbContext>(
     options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 
 var app = builder.Build();
