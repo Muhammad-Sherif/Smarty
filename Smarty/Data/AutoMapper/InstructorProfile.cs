0b6948356eab275774f7156948a15c05385dee9f
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-02-07 17:27:08 +0200

    Add instructor auto mapper profile

---
+++ Smarty/Data/AutoMapper/InstructorProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Smarty.Data.Models;
+using Smarty.Data.ViewModels.Instructors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smarty.Data.AutoMapper
+{
+	public class InstructorProfile : Profile
+	{
+		public InstructorProfile()
+		{
+			CreateMap<InstructorInfoViewModel, Member>().As<Instructor>();
+			CreateMap<InstructorInfoViewModel, Instructor>();
+			CreateMap<InstructorRegisterFormViewModel, SmartyUser>()
+					.ForMember(dest => dest.Member, options => options.MapFrom(src => src.InstructorInfo))
+					.ForMember(dest => dest.UserName, options => options.MapFrom(src => src.Email));
+
+		}
+	}
+}
