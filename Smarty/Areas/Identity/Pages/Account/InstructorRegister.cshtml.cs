a2f5dc2eff5b87f6d270314d4ee89f2d2dad5576
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-02-07 17:25:55 +0200

    Add instructor register page

---
+++ Smarty/Areas/Identity/Pages/Account/InstructorRegister.cshtml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using Smarty.Data.Enums;
+using Smarty.Data.Models;
+using Smarty.Data.ViewModels.Instructors;
+
+namespace Smarty.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class InstructorRegisterModel : PageModel
+    {
+        private readonly SignInManager<SmartyUser> _signInManager;
+        private readonly UserManager<SmartyUser> _userManager;
+        private readonly ILogger<InstructorRegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+        private readonly IMapper _mapper;
+
+        public InstructorRegisterModel(
+            UserManager<SmartyUser> userManager,
+            SignInManager<SmartyUser> signInManager,
+            ILogger<InstructorRegisterModel> logger,
+            IEmailSender emailSender,
+            IMapper mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+			_mapper = mapper;
+		}
+
+        [BindProperty]
+        public InstructorRegisterFormViewModel ViewModel { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+
+
+		public  void OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            if (ModelState.IsValid)
+			{
+				//var user = _mapper.Map<SmartyUser>(ViewModel);
+				var user = _mapper.Map<SmartyUser>(ViewModel);
+				
+				var result = await _userManager.CreateAsync(user, ViewModel.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(ViewModel.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = ViewModel.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
---
+++ Smarty/Data/ViewModels/Instructors/InstructorInfoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Smarty.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smarty.Data.ViewModels.Instructors
+{
+	public class InstructorInfoViewModel
+	{
+
+        [Required]
+        [MaxLength(250)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string LastName { get; set; }
+
+        
+        [Required]
+        public Gender Gender { get; set; }
+
+    }
+}
