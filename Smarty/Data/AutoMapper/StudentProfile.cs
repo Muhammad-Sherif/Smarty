d8c62f3b7e5859670c4cd2ad74fd6749b989be41
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-02-07 17:29:03 +0200

    Add student auto mapper profile

---
+++ Smarty/Data/AutoMapper/StudentProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Smarty.Data.Models;
+using Smarty.Data.ViewModels.Students;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smarty.Data.AutoMapper
+{
+    public class StudentProfile : Profile
+    {
+        public StudentProfile()
+        {
+            CreateMap<StudentInfoViewModel, Member>().As<Student>();
+            CreateMap<StudentInfoViewModel, Student>();
+            CreateMap<StudentRegisterFormViewModel, SmartyUser>()
+                .ForMember(dest => dest.Member, options => options.MapFrom(src => src.StudentInfo))
+                .ForMember(dest => dest.UserName, options => options.MapFrom(src => src.Email));
+                
+
+
+        }
+
+	}
+}
