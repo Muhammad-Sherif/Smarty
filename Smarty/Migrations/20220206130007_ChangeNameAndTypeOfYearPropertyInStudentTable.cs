f5dd373c1a873d36316f51cd1da271b364cd37c6
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-02-23 22:33:13 +0200

    Fix migration bug

--- Smarty/Migrations/20220206130007_ChangeNameAndTypeOfYearPropertyInStudentTable.cs
+++ Smarty/Migrations/20220206130007_ChangeNameAndTypeOfYearPropertyInStudentTable.cs
@@ -8,30 +8,30 @@ namespace Smarty.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // commented because it applying to database before but it migration not exist 
-            //migrationBuilder.DropColumn(
-            //    name: "Year",
-            //    table: "Members");
 
-            //migrationBuilder.AddColumn<string>(
-            //    name: "UniversityYear",
-            //    table: "Members",
-            //    type: "nvarchar(250)",
-            //    maxLength: 250,
-            //    nullable: true);
-        }
+			migrationBuilder.DropColumn(
+				name: "Year",
+				table: "Members");
+
+			migrationBuilder.AddColumn<string>(
+				name: "UniversityYear",
+				table: "Members",
+				type: "nvarchar(250)",
+				maxLength: 250,
+				nullable: true);
+		}
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.DropColumn(
-            //    name: "UniversityYear",
-            //    table: "Members");
+			migrationBuilder.DropColumn(
+				name: "UniversityYear",
+				table: "Members");
 
-            //migrationBuilder.AddColumn<int>(
-            //    name: "Year",
-            //    table: "Members",
-            //    type: "int",
-            //    nullable: true);
-        }
+			migrationBuilder.AddColumn<int>(
+				name: "Year",
+				table: "Members",
+				type: "int",
+				nullable: true);
+		}
     }
 }
