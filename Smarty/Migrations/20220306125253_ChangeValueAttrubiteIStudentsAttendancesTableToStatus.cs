8258230545975545c631725a418e9feaf70bbf12
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-03-06 14:59:44 +0200

    Change value attrubite to status in courses attendances mdoel

---
+++ Smarty/Migrations/20220306125253_ChangeValueAttrubiteIStudentsAttendancesTableToStatus.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Smarty.Migrations
+{
+    public partial class ChangeValueAttrubiteIStudentsAttendancesTableToStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Value",
+                table: "StudentsAttendances",
+                newName: "Status");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "StudentsAttendances",
+                newName: "Value");
+        }
+    }
+}
