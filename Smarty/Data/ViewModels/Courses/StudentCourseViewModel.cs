416811f41b66d6e8f022ee40c6994c42bfc812d7
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-03-12 16:39:21 +0200

    Add student courses page

---
+++ Smarty/Data/ViewModels/Courses/StudentCourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Smarty.Data.ViewModels.Courses
+{
+	public class StudentCourseViewModel
+	{
+		public int Id{ get; set; }
+		public string Code { get; set; }
+		public string Description { get; set; }
+		public string Name { get; set; }
+		public DayOfWeek Day { get; set; }
+		public TimeSpan Time { get; set; }
+	}
+}
---
+++ Smarty/Pages/StudentCourses/Index.cshtml.cs
@@ -0,0 +1,38 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NToastNotify;
+using Smarty.Data.Models;
+using Smarty.Data.Repositories.Interfaces;
+using Smarty.Data.ViewModels.Courses;
+
+namespace Smarty.Pages.StudentCourses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _context;
+        private readonly IToastNotification _toastr;
+        private readonly UserManager<SmartyUser> _userManager;
+        public IndexModel (IUnitOfWork context, IMapper mapper, UserManager<SmartyUser> userManager, IToastNotification toastr)
+        {
+            _mapper = mapper;
+            _context = context;
+            _userManager = userManager;
+            _toastr = toastr;
+        }
+        public IEnumerable<StudentCourseViewModel> ViewModel { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var studentId = _userManager.GetUserAsync(User).Result.MemberId;
+            var studentCourses= await _context.StudentsCourses.FindByCriteriaAsync(c => c.StudentId == studentId , c=>c.Course);
+            var courses = studentCourses.Select(c => c.Course);
+            ViewModel = _mapper.Map<IEnumerable<StudentCourseViewModel>>(courses);
+
+        }
+        
+
+    }
+}
