0275916218f0a9e8622aa91973ed3ac794f9abf2
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-03-17 16:08:17 +0200

    Complete create course attendance page

--- Smarty/Pages/CourseAttendances/Create.cshtml.cs
+++ Smarty/Pages/CourseAttendances/Create.cshtml.cs
@@ -12,8 +12,8 @@ using Smarty.Data.ViewModels.CourseGrades;
 
 namespace Smarty.Pages.CourseAttendances
 {
-    public class CreateModel : PageModel
-    {
+	public class CreateModel : PageModel
+	{
 		private readonly IMapper _mapper;
 		private readonly IUnitOfWork _context;
 		private readonly IToastNotification _toastr;
@@ -26,48 +26,61 @@ namespace Smarty.Pages.CourseAttendances
 			_userManager = userManager;
 			_toastr = toastr;
 			_instuctorService = instuctorService;
+
 		}
 		[BindProperty]
 		public CourseAttendanceFormViewModel ViewModel { get; set; }
+		public string LocationAccessErrorUrl { get; set; }
 		public SelectList SelectList { get; set; }
 		public async Task<IActionResult> OnGetAsync()
 		{
+
 			var instructorId = _userManager.GetUserAsync(User).Result.MemberId;
 			SelectList = await _instuctorService.GetCoursesSelectListAsync(instructorId);
+			LocationAccessErrorUrl = Url.Page(
+			pageName: "/CourseAttendances/LocationAccessError",
+			pageHandler: null,
+			values: null,
+			protocol: Request.Scheme);
 			return Page();
 		}
-        public async Task<IActionResult> OnPostAsync()
-        {
+		public async Task<IActionResult> OnPostAsync()
+		{
 
-            var instructorId = _userManager.GetUserAsync(User).Result.MemberId;
-            SelectList = await _instuctorService.GetCoursesSelectListAsync(instructorId);
+			if(ViewModel.Latitude == 0 || ViewModel.Longitude== 0)
+			{
+				return RedirectToPage("/CourseAttendances/LocationAccessError",new { codeError = 1 });
+			}
 
-            if (!ModelState.IsValid)
-                return Page();
+			if (!ModelState.IsValid)
+				return Page();
 
-            var existedCourse = await _context.Courses.FirstOrDefaultAsync(c => c.Id == ViewModel.CourseId && c.InstructorId == instructorId);
-            if (existedCourse == null)
-            {
-                ModelState.AddModelError(String.Empty, "Course not exist");
-                return Page();
-            }
+			var instructorId = _userManager.GetUserAsync(User).Result.MemberId;
+			SelectList = await _instuctorService.GetCoursesSelectListAsync(instructorId);
+
+			var existedCourse = await _context.Courses.FirstOrDefaultAsync(c => c.Id == ViewModel.CourseId && c.InstructorId == instructorId);
+			if (existedCourse == null)
+			{
+				ModelState.AddModelError(String.Empty, "Course not exist");
+				return Page();
+			}
 
 			var existedCourseAttendance =
 				await _context.CourseAttendances.FindByKeyAsync(ViewModel.DateTime, ViewModel.CourseId);
 
 			if (existedCourseAttendance != null)
-            {
+			{
 				ModelState.AddModelError(String.Empty, "Course Attendance is exist");
 				return Page();
 			}
 
-            var courseAttendance = _mapper.Map<CourseAttendance>(ViewModel);
-			
-            _context.CourseAttendances.Add(courseAttendance);
-            _context.SaveChanges();
-            _toastr.AddSuccessToastMessage("Course Attendance Added Successfully");
-            return RedirectToPage("/CourseAttendances/Index", new { selectedCourseId = ViewModel.CourseId });
+			var courseAttendance = _mapper.Map<CourseAttendance>(ViewModel);
 
-        }
-    }
+			_context.CourseAttendances.Add(courseAttendance);
+			_context.SaveChanges();
+			_toastr.AddSuccessToastMessage("Course Attendance Added Successfully");
+			return RedirectToPage("/CourseAttendances/Index", new { selectedCourseId = ViewModel.CourseId });
+
+		}
+	}
 }
